2a7291f4898f21244a2bff0cf9d7e3362b66bd85
Mihael <mvmihov@gmail.com>
2021-07-27 19:15:52 +0300

    adding files

---
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    // Create User - root/root
+    @Autowired
+    public String[] configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        String[] roles = { "USER", "ADMIN" };
+        auth.inMemoryAuthentication().withUser("root").password("{noop}root").roles(roles[0], roles[1]);
+        return roles;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/login").permitAll().antMatchers("/", "/*exchange*/**","/*trip*/**")
+                .access("hasRole('USER')");
+        super.configure(http);
+    }
+}
