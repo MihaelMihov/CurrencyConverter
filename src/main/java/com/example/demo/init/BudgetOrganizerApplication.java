e1d4f3bba219730830c2afc9010cc836668b7912
MihaelMihov <88043589+MihaelMihov@users.noreply.github.com>
2021-07-29 19:40:28 +0300

    adding files

--- src/main/java/com/example/demo/init/BudgetOrganizerApplication.java
+++ src/main/java/com/example/demo/init/BudgetOrganizerApplication.java
@@ -10,8 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @ComponentScan("com.example.demo.*")
